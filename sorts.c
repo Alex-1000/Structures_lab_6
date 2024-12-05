3e08e879c23023b09204d25f48194704a058a30b
Alex-1000 <neijr@proton.me>
2024-12-05 22:49:58 +0300

    Removed iteration files + bug fixes

--- sorts.c
+++ sorts.c
@@ -1,9 +1,13 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#define WINDOWS
 #include <stdio.h>
+#define read _read
 #define ftruncate _chsize
 #define fileno _fileno
 #else
@@ -12,17 +16,54 @@
 #endif
 
 #define DEFAULT_ALLOC_SIZE 4
+#define BUFFER_SIZE 8192
+
+// Скопировать source в destination, перезаписывая destination при необходимости
+void copyFile(char* source, char* destination) {
+#ifdef WINDOWS
+    CopyFile(source, destination, FALSE)
+#else
+    FILE* src = fopen(source, "r");
+    if (src == NULL) {
+        printf("Возникла ошибка при чтении файла %s\n", source);
+        exit(1);
+    }
+    FILE* dst = fopen(destination, "w");
+    if (dst == NULL) {
+        printf("Возникла ошибка при записи в файл %s\n", destination);
+        exit(2);
+    }
+    int src_fd = fileno(src);
+    int dst_fd = fileno(dst);
+
+    char buffer[BUFFER_SIZE];
+    ssize_t count = 0;
+    while ((count = read(src_fd, buffer, BUFFER_SIZE)) > 0)
+        write(dst_fd, buffer, count);
+
+    fclose(src);
+    fclose(dst);
+    if (count < 0) {
+        printf("Возникла ошибка при копировании файла %s в файл %s", source,
+               destination);
+        exit(10);
+    }
+#endif
+}
 
 // Сортировка естественным слиянием
-void extMergeSort(char* array_file, char* tmp_file_A, char* tmp_file_B) {
+// void extMergeSort(char* array_file, char* tmp_file_A, char* tmp_file_B) {
+void extMergeSort(char* input_file, char* result_file) {
     size_t sequence_count; // Количество отсортированных последовательностей
     sequence_count = 2; // Бессмысленное значение для запуска цикла
 
+    // Копируем входные данные в файл, который будет меняться (в конце - файл с
+    // результатом)
+    copyFile(input_file, result_file);
     // Сортировка будет происходить до тех пор, пока массив не будет
     // отсортирован, то есть не останется одной отсортированной
     // последовательности
-    size_t iteration = 1;
-    for (;; iteration++) {
+    while (sequence_count > 1) {
         sequence_count = 1;
         size_t numbers_count = 1; // Количество чисел
 
@@ -31,8 +72,8 @@ void extMergeSort(char* array_file, char* tmp_file_A, char* tmp_file_B) {
         // Количество длин последовательностей, под которое выделена память
         size_t sequence_len_alloc = DEFAULT_ALLOC_SIZE;
 
-        // Отсортированные последовательности чисел
-        double* sequences = (double*)malloc(sizeof(double) * numbers_alloc);
+        // Числа массива
+        double* numbers = (double*)malloc(sizeof(double) * numbers_alloc);
         // Длины последовательностей
         size_t* sequence_lengths =
             (size_t*)calloc(sequence_len_alloc, sizeof(size_t));
@@ -43,18 +84,7 @@ void extMergeSort(char* array_file, char* tmp_file_A, char* tmp_file_B) {
         char* num_str = malloc(0);           // Считанная строка
 
         // Открываем исходный файл
-        // Название файла с предыдущей последовательностью
-        char* prev_array_file;
-        if (iteration == 1) {
-            size_t len = strlen(array_file) + 1;
-            prev_array_file = (char*)malloc(sizeof(char) * len);
-            memcpy(prev_array_file, array_file, len);
-        } else {
-            prev_array_file = (char*)malloc(
-                sizeof(char) * (strlen(array_file) + 2 + (iteration - 1) % 10));
-            sprintf(prev_array_file, "%zu_%s", iteration - 1, array_file);
-        }
-        FILE* file = fopen(prev_array_file, "r"); // Поток исходного файла
+        FILE* file = fopen(result_file, "r"); // Поток исходного файла
         if (file == NULL) {
             printf("Невозможно открыть исходный файл\n");
             exit(1);
@@ -63,10 +93,10 @@ void extMergeSort(char* array_file, char* tmp_file_A, char* tmp_file_B) {
         // Задаём первый элемент первой последовательности
         if (!getdelim(&num_str, num_size, ' ', file)) {
             printf("Возникла ошибка при чтении исходного файла\n");
-            exit(2);
+            exit(10);
         }
         num = atof(num_str);
-        sequences[0] = num;
+        numbers[0] = num;
         sequence_lengths[0] = 1;
 
         // Читаем до конца файла
@@ -78,11 +108,11 @@ void extMergeSort(char* array_file, char* tmp_file_A, char* tmp_file_B) {
             // необходимости
             if (++numbers_count >= numbers_alloc) {
                 numbers_alloc *= 2;
-                sequences =
-                    (double*)realloc(sequences, sizeof(double) * numbers_alloc);
+                numbers =
+                    (double*)realloc(numbers, sizeof(double) * numbers_alloc);
             }
             // Если число >= предыдущего, оно входит в последовательность,
-            if (num >= sequences[i++]) {
+            if (num >= numbers[i++]) {
                 sequence_lengths[seq]++;
             } else { // иначе оно начинает новую последовательность
                 // Увеличиваем размер массива длин последовательностей по мере
@@ -94,36 +124,24 @@ void extMergeSort(char* array_file, char* tmp_file_A, char* tmp_file_B) {
                 }
                 sequence_lengths[++seq] = 1;
             }
-            sequences[i] = num;
+            numbers[i] = num;
 
             // Выход из цикла при окончании файла
             if (feof(file))
                 break;
         }
-
-        // Закрываем файл
         fclose(file);
-        free(prev_array_file);
 
         // Если есть всего одна последовательность, то массив уже отсортирован
         if (sequence_count <= 1)
             break;
 
         // Распределяем последовательности
-        // Имя первого файла
-        char* tmp_file_A_name = (char*)malloc(
-            sizeof(char) * (strlen(tmp_file_A) + 2 + iteration % 10));
-        sprintf(tmp_file_A_name, "%zu_%s", iteration, tmp_file_A);
-        // Имя второго файла
-        char* tmp_file_B_name = (char*)malloc(
-            sizeof(char) * (strlen(tmp_file_B) + 2 + iteration % 10));
-        sprintf(tmp_file_B_name, "%zu_%s", iteration, tmp_file_B);
-
-        FILE* tmp_A = fopen(tmp_file_A_name, "w"); // Поток первого файла
-        FILE* tmp_B = fopen(tmp_file_B_name, "w"); // Поток второго файла
+        FILE* tmp_A = tmpfile(); // Поток первого файла
+        FILE* tmp_B = tmpfile(); // Поток второго файла
         if (tmp_A == NULL || tmp_B == NULL) {
             printf("Возникла ошибка при открытии вспомогательного файла\n");
-            exit(10);
+            exit(3);
         }
 
         // Заполняем промежуточные файлы
@@ -135,7 +153,7 @@ void extMergeSort(char* array_file, char* tmp_file_A, char* tmp_file_B) {
         size_t seq_i = 0;
         for (seq = 0; seq < sequence_count; seq++) {
             for (i = 0; i < sequence_lengths[seq]; i++)
-                *current_len += fprintf(current, "%f ", sequences[i + seq_i]);
+                *current_len += fprintf(current, "%f ", numbers[i + seq_i]);
             seq_i += sequence_lengths[seq];
 
             fprintf(current, "| ");
@@ -155,30 +173,26 @@ void extMergeSort(char* array_file, char* tmp_file_A, char* tmp_file_B) {
         fflush(tmp_B);
         ftruncate(fileno(tmp_B), length_B - 2);
         // Заканчиваем запись в файлы
-        fclose(tmp_A);
-        fclose(tmp_B);
+        // (используется fflush вместо fclose, так как файлы будут необходимы
+        // позже)
+        fflush(tmp_A);
+        fflush(tmp_B);
 
         // Освобождаем память, выделенную под числа
-        free(sequences);
+        free(numbers);
         free(sequence_lengths);
 
         // Сливаем последовательности
-        // Название файла с промежуточной последовательностью
-        char* new_array_file = (char*)malloc(
-            sizeof(char) * (strlen(array_file) + 2 + iteration % 10));
-        sprintf(new_array_file, "%zu_%s", iteration, array_file);
-        file = fopen(new_array_file, "w");
-        tmp_A = fopen(tmp_file_A_name, "r");
-        tmp_B = fopen(tmp_file_B_name, "r");
+        file = fopen(result_file, "w");
         current = tmp_A;
         if (file == NULL) {
             printf("Невозможно открыть исходный файл\n");
             exit(1);
         }
-        if (tmp_A == NULL || tmp_B == NULL) {
-            printf("Возникла ошибка при открытии вспомогательного файла\n");
-            exit(10);
-        }
+
+        // Ставим указатели во временных файлах в начало
+        fseek(tmp_A, 0, SEEK_SET);
+        fseek(tmp_B, 0, SEEK_SET);
 
         // Считываем первые числа (для сравнения)
         double num_A; // Последнее прочитанное число из файла A
@@ -192,110 +206,111 @@ void extMergeSort(char* array_file, char* tmp_file_A, char* tmp_file_B) {
         int eof_A;
         int eof_B;
         while (!((eof_A = feof(tmp_A)) & (eof_B = feof(tmp_B)))) {
-            if (!eof_A && num_A <= num_B) {
+            if (num_A <= num_B) {
+                // Записываем число в файл и читаем следующее число
                 fprintf(file, "%f ", num_A);
-                if (getdelim(&num_str, num_size, ' ', tmp_A)) {
-                    // Если мы дошли до разделителя в файле A, то дальше можно
-                    // сразу записать отсортированную последовательность из
-                    // файла B
-                    if (strcmp(num_str, "| ") == 0) {
-                        // Читаем до разделителя "| " (или до EOF) в файле B
-                        while (getdelim(&num_str, num_size, ' ', tmp_B) &&
-                               !feof(tmp_B)) {
-                            // Записываем число
-                            fprintf(file, "%f ", num_B);
-                            // Если прочитали разделитель, то берём следующее за
-                            // ним число и выходим из цикла (последовательность
-                            // закончилась)
-                            if (strcmp(num_str, "| ") == 0) {
-                                getdelim(&num_str, num_size, ' ', tmp_B);
-                                num_B = atof(num_str);
-                                break;
-                            }
-                            // Иначе продолжаем читать файл B
+                getdelim(&num_str, num_size, ' ', tmp_A);
+                // Если мы дошли до разделителя в файле A, то дальше можно
+                // сразу записать отсортированную последовательность из
+                // файла B
+                if (strcmp(num_str, "| ") == 0) {
+                    // Читаем до разделителя "| " (или до EOF) в файле B
+                    while (getdelim(&num_str, num_size, ' ', tmp_B) &&
+                           !feof(tmp_B)) {
+                        // Записываем число
+                        fprintf(file, "%f ", num_B);
+                        // Если прочитали разделитель, то берём следующее за
+                        // ним число и выходим из цикла (последовательность
+                        // закончилась)
+                        if (strcmp(num_str, "| ") == 0) {
+                            getdelim(&num_str, num_size, ' ', tmp_B);
                             num_B = atof(num_str);
+                            break;
                         }
-                        // Считываем следующее за разделителем число из файла B
-                        if (getdelim(&num_str, num_size, ' ', tmp_B))
-                            num_B = atof(num_str);
+                        // Иначе продолжаем читать файл B
+                        num_B = atof(num_str);
                     }
-
-                    // Иначе мы продолжаем слияние
-                    else
-                        num_A = atof(num_str);
-                }
-            } else if (!eof_B) {
-                fprintf(file, "%f ", num_B);
-                if (getdelim(&num_str, num_size, ' ', tmp_B)) {
-                    // Если мы дошли до разделителя в файле B, то дальше можно
-                    // сразу записать отсортированную последовательность из
-                    // файла A
-                    if (strcmp(num_str, "| ") == 0) {
-                        // Читаем до разделителя "| " (или до EOF) в файле A
-                        while (getdelim(&num_str, num_size, ' ', tmp_A) &&
-                               !feof(tmp_A)) {
-                            // Записываем число
-                            fprintf(file, "%f ", num_A);
-                            // Если прочитали разделитель, то берём следующее за
-                            // ним число и выходим из цикла (последовательность
-                            // закончилась)
-                            if (strcmp(num_str, "| ") == 0) {
-                                getdelim(&num_str, num_size, ' ', tmp_A);
-                                num_A = atof(num_str);
-                                break;
-                            }
-                            // Иначе продолжаем читать файл A
-                            num_A = atof(num_str);
-                        }
-                        // Считываем следующее за разделителем число из файла B
-                        if (getdelim(&num_str, num_size, ' ', tmp_B))
-                            num_B = atof(num_str);
-                    }
-                    // Иначе мы продолжаем слияние
-                    else
+                    // Считываем следующее за разделителем число из файла B
+                    if (getdelim(&num_str, num_size, ' ', tmp_B))
                         num_B = atof(num_str);
                 }
+                // Если мы достигли конца файла, считаем числа из файла B меньше
+                // числа из файла A (для этого приравниваем это число
+                // бесконечности)
+                else if (eof_A)
+                    num_A = INFINITY;
+                // Иначе мы берём следующее число и продолжаем слияние
+                else
+                    num_A = atof(num_str);
+
+            } else {
+                // Записываем число в файл и читаем следующее число
+                fprintf(file, "%f ", num_B);
+                getdelim(&num_str, num_size, ' ', tmp_B);
+                // Если мы дошли до разделителя в файле B, то дальше можно
+                // сразу записать отсортированную последовательность из
+                // файла A
+                if (strcmp(num_str, "| ") == 0) {
+                    // Читаем до разделителя "| " (или до EOF) в файле A
+                    while (getdelim(&num_str, num_size, ' ', tmp_A) &&
+                           !feof(tmp_A)) {
+                        // Записываем число
+                        fprintf(file, "%f ", num_A);
+                        // Если прочитали разделитель, то берём следующее за
+                        // ним число и выходим из цикла (последовательность
+                        // закончилась)
+                        if (strcmp(num_str, "| ") == 0) {
+                            getdelim(&num_str, num_size, ' ', tmp_A);
+                            num_A = atof(num_str);
+                            break;
+                        }
+                        // Иначе продолжаем читать файл A
+                        num_A = atof(num_str);
+                    }
+                    // Считываем следующее за разделителем число из файла B
+                    if (getdelim(&num_str, num_size, ' ', tmp_B))
+                        num_B = atof(num_str);
+                }
+                // Если мы достигли конца файла, считаем числа из файла A меньше
+                // числа из файла B (для этого приравниваем это число
+                // бесконечности)
+                else if (eof_B)
+                    num_B = INFINITY;
+                // Иначе мы продолжаем слияние
+                else
+                    num_B = atof(num_str);
             }
             fflush(file);
         }
-        // Записываем 2 последних числа (цикл закончился из-за EOF)
-        if (num_A <= num_B)
-            fprintf(file, "%f %f", num_A, num_B);
-        else
-            fprintf(file, "%f %f", num_B, num_A);
+        // При необходимости записываем 2 последних числа
+        if (!isinf(num_A) && !isinf(num_B)) {
+            if (num_A <= num_B)
+                fprintf(file, "%f %f", num_A, num_B);
+            else
+                fprintf(file, "%f %f", num_B, num_A);
+        } else if (!isinf(num_A))
+            fprintf(file, "%f", num_A);
+        else if (!isinf(num_A))
+            fprintf(file, "%f", num_B);
+
         // Прекращаем работу с файлами и освобождаем память
         fclose(file);
         fclose(tmp_A);
         fclose(tmp_B);
-        free(new_array_file);
-        free(tmp_file_A_name);
-        free(tmp_file_B_name);
-    }
-
-    // Переименование результирующего файла
-    if (iteration > 1) {
-        char* old_name = (char*)malloc(
-            sizeof(char) * (strlen(array_file) + 2 + (iteration - 1) % 10));
-        sprintf(old_name, "%zu_%s", iteration - 1, array_file);
-        rename(old_name, "result");
     }
 }
 
 int main(int argc, char* argv[]) {
     // Имена файлов
-    char* file_path = malloc(0); // Имя начального файла
-    char* tmp_file_A = malloc(sizeof(char) * 5); // Имя первого временного файла
-    char* tmp_file_B = malloc(sizeof(char) * 5); // Имя второго временного файла
-    // Стандартные пути для временных файлов
-    memcpy(tmp_file_A, "tmp_A", 5);
-    memcpy(tmp_file_B, "tmp_B", 5);
+    char* input_file = malloc(0); // Имя начального файла
+    char* result_file = malloc(sizeof(char) * 7); // Имя файла с результатом
+    memcpy(result_file, "result", 7);
 
     // Работа с аргументами
     if (argc > 1) {
-        file_path = argv[1];
-        if (argc > 3) {
-            tmp_file_A = argv[2];
-            tmp_file_B = argv[3];
+        input_file = argv[1];
+        if (argc > 2) {
+            result_file = argv[2];
         }
     }
     // Работа с клавиатурой
@@ -310,34 +325,25 @@ int main(int argc, char* argv[]) {
         getline(&line, line_size, stdin);
         line_len = strlen(line);
         while (line_len <= 1) {
-            printf("Укажите путь > ");
+            printf("Имя файла: > ");
             getline(&line, line_size, stdin);
             line_len = strlen(line);
         }
         line[line_len - 1] = '\0';
-        strcpy(file_path, line);
-        // Ввод имён временных файлов
-        printf("Введите имена промежуточных файлов (или нажмите Enter, чтобы "
-               "оставить имя по умолчанию)\n");
-        printf("Имя первого промежуточного файла:\n> ");
+        strcpy(input_file, line);
+        // Ввод имени результирующего файла
+        printf("Введите имя файла с результатом (или нажмите Enter, чтобы "
+               "оставить имя по умолчанию)\n> ");
         getline(&line, line_size, stdin);
         line_len = strlen(line);
         if (line_len > 1) {
             line[line_len - 1] = '\0';
-            strcpy(tmp_file_A, line);
-        }
-        printf("Имя второго промежуточного файла:\n> ");
-        getline(&line, line_size, stdin);
-        line_len = strlen(line);
-        if (line_len > 1) {
-            line[line_len - 1] = '\0';
-            strcpy(tmp_file_B, line);
+            strcpy(result_file, line);
         }
     }
 
-    extMergeSort(file_path, tmp_file_A, tmp_file_B);
+    extMergeSort(input_file, result_file);
 
-    free(file_path);
-    free(tmp_file_A);
-    free(tmp_file_B);
+    free(input_file);
+    free(result_file);
 }
